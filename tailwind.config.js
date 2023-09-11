cb36a99fb1dd05fca73f0398e91779b3f7752928
marsrian <marsrian40@gmail.com>
2023-09-12 02:07:22 +0600

    landguru project plac section added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['DM Sans', 'ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+    }
+  },
+  plugins: [],
+}
+
